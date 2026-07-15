7ce3f43a4bdd5b3e990916ca9ed5f26ced7e1970
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-15 23:27:31 +0600

    feat(js-practice): Student Information

---
+++ Java Script/Javascript Practice Task/Task 03 - Student Information/app.js
@@ -0,0 +1,9 @@
+let studentName = "Foysal Hossien";
+let age = 24;
+let country = "Bangladesh";
+let isStudent = true;
+
+console.log(`Student Name: ${studentName}`);
+console.log(`Age: ${age}`);
+console.log(`Country: ${country}`);
+console.log(`Is Student: ${isStudent}`);
