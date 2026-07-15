7b7f7d63337092d8e8d82ff0ef1f2f158dc111be
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-15 23:49:53 +0600

    feat(js-practice): Indetify Data Types

---
+++ Java Script/Javascript Practice Task/Task 04 - Identify Data Types/app.js
@@ -0,0 +1,17 @@
+let a = 100;
+let b = "Programming";
+let c = true;
+let d = false;
+let e = 7.3533;
+let f;
+let g = "";
+let h = null;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
+console.log(typeof h);
