c7d34002126e330e493e5f92825f6136523ec667
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-18 02:56:06 +0600

    feat(js): determine season based on month number

---
+++ Java Script/Javascript Practice Task/Task 22 - Season Checker/app.js
@@ -0,0 +1,17 @@
+const month = 5;
+
+if (month === 1 || month === 2) {
+  console.log("Summer");
+} else if (month === 3 || month === 4) {
+  console.log("Monsoon");
+} else if (month === 5 || month === 6) {
+  console.log("Autumn");
+} else if (month === 7 || month === 8) {
+  console.log("Late Autumn");
+} else if (month === 9 || month === 10) {
+  console.log("Winter");
+} else if (month === 11 || month === 12) {
+  console.log("Spring");
+} else {
+  console.log("Invalid Season");
+}
