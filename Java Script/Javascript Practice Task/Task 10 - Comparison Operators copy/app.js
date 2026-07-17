d9e7f930aeeef300236366c3b266f8e963127e5d
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-18 00:11:43 +0600

    feat(js): Modified comparison operators practice

---
+++ Java Script/Javascript Practice Task/Task 10 - Comparison Operators copy/app.js
@@ -0,0 +1,31 @@
+const price1 = 25;
+const price2 = 20;
+
+if (price1 == price2) {
+  console.log("Values are the same");
+}
+
+if (price1 === price2) {
+  console.log("Value and data type are same");
+}
+
+if (price1 != price2) {
+  console.log("Values are not the same");
+}
+if (price1 !== price2) {
+  console.log("Value and data type are not same");
+}
+
+if (price1 > price2) {
+  console.log("price1 is greater than price2");
+}
+if (price1 < price2) {
+  console.log("price1 is less than price2");
+}
+
+if (price1 >= price2) {
+  console.log("price1 is greater or equal than price2");
+}
+if (price1 <= price2) {
+  console.log("price1 is less or equal than price2");
+}
