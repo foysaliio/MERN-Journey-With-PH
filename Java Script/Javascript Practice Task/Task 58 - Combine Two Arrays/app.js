5031f2622a88b0c0540a574ab32fcdfff89a8bd9
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-21 20:11:27 +0600

    feat(js): combine two arrays into a new array

---
+++ Java Script/Javascript Practice Task/Task 58 - Combine Two Arrays/app.js
@@ -0,0 +1,10 @@
+const schoolFriend = ["Faruk", "Shakil", "Musa", "Rakib"];
+
+const polytechnicFriends = ["Amit", "Rayhan", "Ifti", "Himu"];
+
+console.log(schoolFriend);
+console.log(polytechnicFriends);
+
+const allFriend = schoolFriend.concat(polytechnicFriends);
+
+console.log(allFriend);
