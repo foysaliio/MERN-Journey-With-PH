889a03c5b3aae9aa9d86c58bff93ea2002baf2b7
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-23 03:30:41 +0600

    feat(js): practice combining strings

---
+++ Java Script/Javascript Practice Task/Task 76 - Combine Strings/app.js
@@ -0,0 +1,10 @@
+const firstWord = "Hello";
+const lastWord = "World";
+
+const firstOutput = firstWord.concat(" ", lastWord);
+const secondOutput = firstWord + " " + lastWord;
+const thirdOutput = `${firstWord} ${lastWord}`;
+
+console.log(firstOutput);
+console.log(secondOutput);
+console.log(thirdOutput);
