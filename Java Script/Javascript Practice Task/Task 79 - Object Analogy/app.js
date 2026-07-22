31fb826b5fdc1f7abe3d7ffd2d00fa570bfa3b3c
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-23 04:05:08 +0600

    docs(js): explain object using a real-life analogy

---
+++ Java Script/Javascript Practice Task/Task 79 - Object Analogy/app.js
@@ -0,0 +1,5 @@
+`// An object is a JavaScript data type that stores information as key-value pairs.
+// It is mutable, which means its values can be changed after it is created.
+// A mobile phone is a good example of an object. It has properties like brand ("iPhone"),
+// model ("17 Pro Max"), color ("Red"), and weight ("250g"). Each property has a value,
+// just like an object in JavaScript.`;
