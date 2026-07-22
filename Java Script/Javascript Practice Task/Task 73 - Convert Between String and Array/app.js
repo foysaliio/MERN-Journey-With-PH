d432802e65dce9c14f6491d893312734bc402e19
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-23 03:05:34 +0600

    feat(js): convert between strings and arrays

---
+++ Java Script/Javascript Practice Task/Task 73 - Convert Between String and Array/app.js
@@ -0,0 +1,8 @@
+const javaScriptStr = "JavaScript";
+const catArray = ["c", "a", "t"];
+
+const javaScriptArray = javaScriptStr.split("");
+console.log(javaScriptArray);
+
+const catStr = catArray.join("");
+console.log(catStr);
